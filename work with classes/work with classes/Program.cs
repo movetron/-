9cbc18bf09ac7a3807e773dddfca39d1afea156c
Назаров Михаил <85027066+movetron@users.noreply.github.com>
2023-01-20 16:04:19 +0300

    Add files via upload

---
+++ work with classes/work with classes/Program.cs
@@ -0,0 +1,10 @@
+﻿using work_with_classes;
+
+Random rand = new Random();
+Triangle[] triang = new Triangle[5];
+for (int i = 0; i < triang.Length; i++)
+{
+    triang[i] = new Triangle(rand.Next(1, 150), rand.Next(1, 150));
+    Console.WriteLine(triang[i]);
+}
+
---
+++ work with classes/work with classes/Triangle.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace work_with_classes
+{
+    internal class Triangle
+    {
+        public Triangle(double ang, double rand)
+        {
+            Ang1 = ang;
+            Ang2 = 180 - ang - rand;
+            Ang3 = 180 - ang - (180 - ang - rand);
+
+        }
+
+        public double Ang1 { get; set; }                          
+        public double Ang2 { get; set; }
+        public double Ang3 { get; set; }
+
+        public override string ToString()
+        {
+            return "(" +Ang1 + "+" + Ang2 + "+" + Ang3+ ")";
+        }
+    }
+    
+
+
+
+}
